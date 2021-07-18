53933e05c037de4b9ad218a4240d43799630d48e
tthheusalmeida <tthheusalmeida@gmail.com>
2021-07-18 09:02:01 -0300

    feat: create api handler

---
+++ front/services/Api.js
@@ -0,0 +1,55 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost:4445';
+export const END_POINT = {
+  LOGIN: '/login',
+  REGISTER: '/register',
+  RECOVER: '/recover',
+  OPERATION: '/operation',
+  REPORT: '/report',
+};
+
+export async function getData(endPoint) {
+  let data = {};
+
+  const baseURL = `${BASE_URL}${endPoint}`;
+  const api = axios.create({
+    baseURL,
+  });
+
+  await api
+    .get()
+    .then((response) => {
+      data = response?.data;
+    })
+    .catch((error) => {
+      console.error(error.message);
+    });
+
+  return data;
+}
+
+export async function postData(options) {
+  const {
+    endPoint,
+    header,
+  } = options;
+
+  let data = {};
+
+  const baseURL = BASE_URL;
+  const api = axios.create({
+    baseURL,
+  });
+
+  await api
+    .post(endPoint, header)
+    .then((response) => {
+      data = response;
+    })
+    .catch((error) => {
+      console.log(error.message);
+    });
+
+  return data;
+}
